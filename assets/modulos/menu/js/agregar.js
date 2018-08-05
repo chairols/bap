f1ac983bc81e013e448357000fe3426e06aaa2f0
Hernan <Hernan@hernans-macbook-pro.local>
2018-08-05 16:58:13 -0300

    - Se corrigió error que no mostraba error en ajax cuando se quería agregar un menú

--- assets/modulos/menu/js/agregar.js
+++ assets/modulos/menu/js/agregar.js
@@ -20,7 +20,7 @@ $("#agregar").click(function() {
                 success: function(data) {
                     resultado = $.parseJSON(data);
                     if (resultado['status'] == 'error') {
-                        alertify.error($resultado['data']);
+                        alertify.error(resultado['data']);
                     } else if (resultado['status'] == 'ok') {
                         alertify.success("Se agregó correctamente.");
                         document.getElementById("icono").value = "";
@@ -34,45 +34,3 @@ $("#agregar").click(function() {
         }
     });
 });
-
-$("#agregar222").click(function () {
-    alertify.confirm("Se agregará el menú <strong>" + $("#menu").val() + "</strong><br><strong>¿Desea confirmar?</strong>", function (e) {
-        if (e)
-        {
-            datos = {
-                'icono': $("#icono").val(),
-                'titulo': $("#titulo").val(),
-                'menu': $("#menu").val(),
-                'href': $("#href").val(),
-                'orden': $("#orden").val(),
-                'padre': $("#padre").val(),
-                'visible': $("#visible").is(':checked')
-            };
-            $.ajax({
-                type: 'POST',
-                url: '/menu/agregar_ajax/',
-                data: datos,
-                beforeSend: function () {
-
-                },
-                success: function (data) {
-                    resultado = $.parseJSON(data);
-                    if (resultado['status'] == 'error') {
-                        alertify.error($resultado['data']);
-                    } else if (resultado['status'] == 'ok') {
-                        alertify.success("Se agregó correctamente.");
-                        document.getElementById("icono").value = "";
-                        document.getElementById("titulo").value = "";
-                        document.getElementById("menu").value = "";
-                        document.getElementById("href").value = "";
-                        document.getElementById("orden").value = "";
-                    }
-                }
-            });
-
-        }
-    });
-
-});
-
-
