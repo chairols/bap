a6f46d31e58a173d744653450994b193f7b16740
Hernan <Hernan@hernans-macbook-pro.local>
2018-08-16 00:02:55 -0300

    - Se arregló la vista del Dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,9 @@
-<pre>
-    <?php print_r($session); ?>
-</pre>
+<div class="col-xs-12">
+    <div class="card">
+        <div class="body">
+            <pre>
+                <?php print_r($session); ?>
+            </pre>
+        </div>
+    </div>
+</div>
