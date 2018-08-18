01789a582ce4eaa5c422f1a1e79f67abff69d06f
Hernan <Hernan@hernans-macbook-pro.local>
2018-08-17 21:36:36 -0300

    - Se modificó pantalla de login

--- application/views/usuarios/login.php
+++ application/views/usuarios/login.php
@@ -1,62 +1,135 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $title ?></title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.6 -->
-        <link rel="stylesheet" href="/assets/AdminLTE-2.4.3/bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="/assets/AdminLTE-2.4.3/bower_components/font-awesome/css/font-awesome.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="/assets/AdminLTE-2.4.3/dist/css/AdminLTE.min.css">
+<!doctype html>
+<html class="no-js " lang="en">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body class="hold-transition login-page">
-        <div class="login-box">
-            <div class="login-logo">
-                <b>LOGIN</b>
-            </div>
-            <!-- /.login-logo -->
-            <div class="login-box-body">
+    <title>:: Oreo Bootstrap4 Admin ::</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/authentication.css">
+    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/color_skins.css">
+</head>
 
-                <form method="post">
-                    <div class="form-group has-feedback">
-                        <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Usuario" autofocus required>
-                        <span class="fa fa-envelope form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
-                        <input type="password" id="password" name="password" class="form-control" placeholder="Contraseña">
-                        <span class="fa fa-lock form-control-feedback"></span>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-4"></div>
-                        <!-- /.col -->
-                        <div class="col-xs-4">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+<body class="theme-purple authentication sidebar-collapse">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top navbar-transparent">
+    <div class="container">        
+        <div class="navbar-translate n_logo">
+            <a class="navbar-brand" href="javascript:void(0);" title="" target="_blank">Oreo</a>
+            <button class="navbar-toggler" type="button">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);">Search Result</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" title="Follow us on Twitter" href="javascript:void(0);" target="_blank">
+                        <i class="zmdi zmdi-twitter"></i>
+                        <p class="d-lg-none d-xl-none">Twitter</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" title="Like us on Facebook" href="javascript:void(0);" target="_blank">
+                        <i class="zmdi zmdi-facebook"></i>
+                        <p class="d-lg-none d-xl-none">Facebook</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" title="Follow us on Instagram" href="javascript:void(0);" target="_blank">                        
+                        <i class="zmdi zmdi-instagram"></i>
+                        <p class="d-lg-none d-xl-none">Instagram</p>
+                    </a>
+                </li>                
+                <li class="nav-item">
+                    <a class="nav-link btn btn-white btn-round" href="javascript:void(0);">SIGN UP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
+<div class="page-header">
+    <div class="page-header-image" style="background-image:url(/assets/oreoadmin-140/html-light/assets/images/login.jpg)"></div>
+    <div class="container">
+        <div class="col-md-12 content-center">
+            <div class="card-plain">
+                <form class="form" method="POST">
+                    <div class="header">
+                        <div class="logo-container">
+                            <img src="/assets/oreoadmin-140/html-light/assets/images/logo.svg" alt="">
                         </div>
-                        <!-- /.col -->
+                        <h5>Log in</h5>
+                    </div>
+                    <div class="content">                                                
+                        <div class="input-group input-lg">
+                            <input type="text" name="usuario" class="form-control" placeholder="Usuario" autofocus>
+                            <span class="input-group-addon">
+                                <i class="zmdi zmdi-account-circle"></i>
+                            </span>
+                        </div>
+                        <div class="input-group input-lg">
+                            <input type="password" name="password" placeholder="Contraseña" class="form-control" />
+                            <span class="input-group-addon">
+                                <i class="zmdi zmdi-lock"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="footer text-center">
+                        <button type="submit" class="btn btn-primary btn-round btn-lg btn-block ">SIGN IN</button>
+                        <h5><a href="javascript:void(0);" class="link">Forgot Password?</a></h5>
                     </div>
                 </form>
-
-
             </div>
-            <!-- /.login-box-body -->
         </div>
-        <!-- /.login-box -->
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="http://thememakker.com/contact/" target="_blank">Contact Us</a></li>
+                    <li><a href="http://thememakker.com/about/" target="_blank">About Us</a></li>
+                    <li><a href="javascript:void(0);">FAQ</a></li>
+                </ul>
+            </nav>
+            <div class="copyright">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>,
+                <span>Designed by <a href="http://thememakker.com/" target="_blank">ThemeMakker</a></span>
+            </div>
+        </div>
+    </footer>
+</div>
 
-        <!-- jQuery 3.3.1 -->
-        <script src="/assets/vendors/jquery/dist/jquery.min.js"></script>
-        <!-- Bootstrap 3.3.7 -->
-        <script src="/assets/template/js/bootstrap.min.js"></script>
+<!-- Jquery Core Js -->
+<script src="/assets/oreoadmin-140/html-light/assets/bundles/libscripts.bundle.js"></script>
+<script src="/assets/oreoadmin-140/html-light/assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js -->
 
-    </body>
+<script>
+   $(".navbar-toggler").on('click',function() {
+    $("html").toggleClass("nav-open");
+});
+//=============================================================================
+$('.form-control').on("focus", function() {
+    $(this).parent('.input-group').addClass("input-group-focus");
+}).on("blur", function() {
+    $(this).parent(".input-group").removeClass("input-group-focus");
+});
+</script>
+</body>
 </html>
