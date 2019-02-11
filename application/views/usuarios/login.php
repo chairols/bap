23598b96e630d592eeec58fdc47a7d5fd83fd0be
chair <chair@HERNAN-ACER>
2019-02-11 11:48:48 -0300

    - varios

--- application/views/usuarios/login.php
+++ application/views/usuarios/login.php
@@ -1,135 +1,84 @@
 <!doctype html>
-<html class="no-js " lang="en">
-<head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+<html lang="en">
 
-    <title>:: Oreo Bootstrap4 Admin ::</title>
-    <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <!-- Custom Css -->
-    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/plugins/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/main.css">
-    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/authentication.css">
-    <link rel="stylesheet" href="/assets/oreoadmin-140/html-light/assets/css/color_skins.css">
+<head>
+<title>:: Mplify :: Login</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Mplify Bootstrap 4.1.1 Admin Template">
+<meta name="author" content="ThemeMakker, design by: ThemeMakker.com">
+
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/animate-css/animate.min.css">
+<link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/font-awesome/css/font-awesome.min.css">
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="/assets/mplifyadmin/html/light/assets/css/main.css">
+<link rel="stylesheet" href="/assets/mplifyadmin/html/light/assets/css/color_skins.css">
 </head>
 
-<body class="theme-purple authentication sidebar-collapse">
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg fixed-top navbar-transparent">
-    <div class="container">        
-        <div class="navbar-translate n_logo">
-            <a class="navbar-brand" href="javascript:void(0);" title="" target="_blank">Oreo</a>
-            <button class="navbar-toggler" type="button">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-            </button>
-        </div>
-        <div class="navbar-collapse">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);">Search Result</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" title="Follow us on Twitter" href="javascript:void(0);" target="_blank">
-                        <i class="zmdi zmdi-twitter"></i>
-                        <p class="d-lg-none d-xl-none">Twitter</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" title="Like us on Facebook" href="javascript:void(0);" target="_blank">
-                        <i class="zmdi zmdi-facebook"></i>
-                        <p class="d-lg-none d-xl-none">Facebook</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" title="Follow us on Instagram" href="javascript:void(0);" target="_blank">                        
-                        <i class="zmdi zmdi-instagram"></i>
-                        <p class="d-lg-none d-xl-none">Instagram</p>
-                    </a>
-                </li>                
-                <li class="nav-item">
-                    <a class="nav-link btn btn-white btn-round" href="javascript:void(0);">SIGN UP</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<!-- End Navbar -->
-<div class="page-header">
-    <div class="page-header-image" style="background-image:url(/assets/oreoadmin-140/html-light/assets/images/login.jpg)"></div>
-    <div class="container">
-        <div class="col-md-12 content-center">
-            <div class="card-plain">
-                <form class="form" method="POST">
-                    <div class="header">
-                        <div class="logo-container">
-                            <img src="/assets/oreoadmin-140/html-light/assets/images/logo.svg" alt="">
+<body class="theme-blue">
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle auth-main">
+				<div class="auth-box">
+                    <div class="mobile-logo"><a href="index.html"><img src="../assets/images/logo-icon.svg" alt="Mplify"></a></div>
+                    <div class="auth-left">
+                        <div class="left-top">
+                            <a href="index.html">
+                                <img src="/assets/mplifyadmin/html/assets/images/logo-icon.svg" alt="Mplify">
+                                <span>Mplify</span>
+                            </a>
                         </div>
-                        <h5>Log in</h5>
-                    </div>
-                    <div class="content">                                                
-                        <div class="input-group input-lg">
-                            <input type="text" name="usuario" class="form-control" placeholder="Usuario" autofocus>
-                            <span class="input-group-addon">
-                                <i class="zmdi zmdi-account-circle"></i>
-                            </span>
-                        </div>
-                        <div class="input-group input-lg">
-                            <input type="password" name="password" placeholder="Contraseña" class="form-control" />
-                            <span class="input-group-addon">
-                                <i class="zmdi zmdi-lock"></i>
-                            </span>
+                        <div class="left-slider">
+                            <img src="/assets/mplifyadmin/html/assets/images/login/1.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="footer text-center">
-                        <button type="submit" class="btn btn-primary btn-round btn-lg btn-block ">SIGN IN</button>
-                        <h5><a href="javascript:void(0);" class="link">Forgot Password?</a></h5>
+                    <div class="auth-right">
+                        <div class="right-top">
+                            <ul class="list-unstyled clearfix d-flex">
+                                <li><a href="index.html"><i class="fa fa-home"></i></a></li>
+                                <li><a href="javascript:void(0);">Help</a></li>
+                                <li><a href="javascript:void(0);">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="card">
+                            <div class="header">
+                                <p class="lead">Log in</p>
+                            </div>
+                            <div class="body">
+                                <form class="form-auth-small" method="POST">
+                                    <div class="form-group">
+                                        <label for="signin-email" class="control-label sr-only">Usuario</label>
+                                        <input type="text" class="form-control" id="signin-email" name="usuario" value="" placeholder="Usuario" autofocus="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="signin-password" class="control-label sr-only">Password</label>
+                                        <input type="password" class="form-control" id="signin-password" name="password" value="" placeholder="Password">
+                                    </div>
+                                    <div class="form-group clearfix">
+                                        <label class="fancy-checkbox element-left">
+                                            <input type="checkbox">
+                                            <span>Remember me</span>
+                                        </label>								
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+                                    <div class="bottom">
+                                        <span class="helper-text m-b-10"><i class="fa fa-lock"></i> <a href="page-forgot-password.html">Forgot password?</a></span>
+                                        <span>Don't have an account? <a href="page-register.html">Register</a></span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <footer class="footer">
-        <div class="container">
-            <nav>
-                <ul>
-                    <li><a href="http://thememakker.com/contact/" target="_blank">Contact Us</a></li>
-                    <li><a href="http://thememakker.com/about/" target="_blank">About Us</a></li>
-                    <li><a href="javascript:void(0);">FAQ</a></li>
-                </ul>
-            </nav>
-            <div class="copyright">
-                &copy;
-                <script>
-                    document.write(new Date().getFullYear())
-                </script>,
-                <span>Designed by <a href="http://thememakker.com/" target="_blank">ThemeMakker</a></span>
-            </div>
-        </div>
-    </footer>
-</div>
-
-<!-- Jquery Core Js -->
-<script src="/assets/oreoadmin-140/html-light/assets/bundles/libscripts.bundle.js"></script>
-<script src="/assets/oreoadmin-140/html-light/assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js -->
-
-<script>
-   $(".navbar-toggler").on('click',function() {
-    $("html").toggleClass("nav-open");
-});
-//=============================================================================
-$('.form-control').on("focus", function() {
-    $(this).parent('.input-group').addClass("input-group-focus");
-}).on("blur", function() {
-    $(this).parent(".input-group").removeClass("input-group-focus");
-});
-</script>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
 </body>
 </html>
