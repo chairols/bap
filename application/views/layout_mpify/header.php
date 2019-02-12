cb7d1a4f9e8e5d99930c6433d99b1766e0aab0c4
chair <chair@HERNAN-ACER>
2019-02-12 11:32:08 -0300

    - Se modifica vista en crear mensaje

--- application/views/layout_mpify/header.php
+++ application/views/layout_mpify/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
     <head>
-        <title>:: Mplify :: Ui Treeview</title>
+        <title>:: Mplify :: Home</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -14,35 +14,35 @@
         <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/animate-css/animate.min.css">
         <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/toastr/toastr.min.css">
-
-        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/bootstrap-treeview/bootstrap-treeview.min.css">
-        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/jstree/themes/default/style.min.css">
-
-        <!-- Select2 -->
-        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/select2/select2.css" />
-
-        <?php if (isset($css) && count($css) > 0) { ?>
-            <!-- Carga de Scripts de la vista -->
-            <?php foreach ($css as $c) { ?>
-                <script type="text/javascript" src="<?= $c ?>"></script>
-                <?php
-            }
-        }
-        ?>
+        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css">
+        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/chartist/css/chartist.min.css">
+        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.css">
+        <link rel="stylesheet" href="/assets/mplifyadmin/html/assets/vendor/summernote/dist/summernote.css"/>
 
         <!-- MAIN CSS -->
         <link rel="stylesheet" href="/assets/mplifyadmin/html/light/assets/css/main.css">
         <link rel="stylesheet" href="/assets/mplifyadmin/html/light/assets/css/color_skins.css">
-
     </head>
+
+
+
+    <?php if (isset($css) && count($css) > 0) { ?>
+        <!-- Carga de Scripts de la vista -->
+        <?php foreach ($css as $c) { ?>
+            <script type="text/javascript" src="<?= $c ?>"></script>
+            <?php
+        }
+    }
+    ?>
+
+
     <body class="theme-blue">
 
         <!-- Page Loader -->
         <div class="page-loader-wrapper">
             <div class="loader">
-                <div class="m-t-30"><img src="../assets/images/thumbnail.png" width="48" height="48" alt="Mplify"></div>
-                <p>Please wait...</p>
+                <div class="m-t-30"><img src="/assets/mplifyadmin/html/assets/images/thumbnail.png" width="48" height="48" alt="Mplify"></div>
+                <p>Please wait...</p>        
             </div>
         </div>
         <!-- Overlay For Sidebars -->
--- application/views/mensajes/crear.php
+++ application/views/mensajes/crear.php
@@ -1,20 +1,47 @@
-<div class="row clearfix">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="body">
-                <div class="row">
-                    <div class="col-md-12 col-lg-12 col-xl-12">
-                        <div class="form-group form-float">
-                            <input class="form-control" placeholder="Para:" type="text" autofocus>
+<div id="main-content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <div class="row">
+                <div class="col-lg-5 col-md-8 col-sm-12">                        
+                    <h2><?= $title ?></h2>
+                </div>            
+                <div class="col-lg-7 col-md-4 col-sm-12 text-right">
+                    <ul class="breadcrumb justify-content-end">
+                        <li class="breadcrumb-item"><a href="index.html"><i class="icon-home"></i></a></li>                            
+                        <li class="breadcrumb-item">UI Elements</li>
+                        <li class="breadcrumb-item active">Treeview</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="row clealfix">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="body">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Para">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Asunto">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="CC">
+                            </div>
+                        </form>
+                        <hr>
+                        <div class="summernote">
+                            Hello there,
+                            <br/>
+                            <p>The toolbar can be customized and it also supports various callbacks such as <code>oninit</code>, <code>onfocus</code>, <code>onpaste</code> and many more.</p>
+                            <p>Please try <b>paste some texts</b> here</p>
                         </div>
-                        <div class="form-group form-float">
-                            <input class="form-control" placeholder="Asunto" type="text">
+                        <div class="m-t-30">
+                            <button type="button" class="btn btn-success">Send Message</button>
+                            <button type="button" class="btn btn-secondary">Draft</button>
+                            <a href="app-inbox.html" class="btn btn-outline-secondary">Cancel</a>
                         </div>
-                    </div>
-                    <div class="col-md-12 col-lg-12 col-xl-12">
-                        <strong>Contenido:</strong>
-                        <textarea id="contenido"></textarea>
-                        <button type="button" class="btn btn-primary btn-round waves-effect m-t-20">Enviar Mensaje</button>
                     </div>
                 </div>
             </div>
